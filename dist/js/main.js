7e851077366d1d558db2a66e6b1cb465e0843841
K1ra-Python <maksimka.guchenko@mail.ru>
2024-07-31 01:40:13 +0300

    Добавил дроп

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,3 +1,20 @@
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Закройте выпадающее меню, если пользователь щелкает за его пределами
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  } 
 let currentIndex = 0;
 
 function showSlide(index) {
