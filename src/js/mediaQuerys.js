be2a6f130ef08e76471fe080dec08d4e6a9a4d11
K1ra-Python <maksimka.guchenko@mail.ru>
2024-08-01 00:50:22 +0300

    #

--- src/js/mediaQuerys.js
+++ src/js/mediaQuerys.js
@@ -1,20 +1,3 @@
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
-  
-  // Закройте выпадающее меню, если пользователь щелкает за его пределами
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
-    }
-  } 
 function myFunction(x) {
     var button = document.getElementById("gfe");
     if (x.matches) {
@@ -79,31 +62,3 @@ function myFunction(x) {
     myFunction(x);
   });
   
-let currentIndex = 0;
-
-function showSlide(index) {
-    const slides = document.querySelector('.slides');
-    const totalSlides = document.querySelectorAll('.slide').length;
-
-    if (index >= totalSlides) {
-        currentIndex = 0;
-    } else if (index < 0) {
-        currentIndex = totalSlides - 1;
-    } else {
-        currentIndex = index;
-    }
-
-    const offset = -currentIndex * 100;
-    slides.style.transform = `translateX(${offset}%)`;
-}
-
-function nextSlide() {
-    showSlide(currentIndex + 1);
-}
-
-function prevSlide() {
-    showSlide(currentIndex - 1);
-}
-
-// Инициализация первого слайда
-showSlide(currentIndex);
